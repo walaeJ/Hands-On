a457b91fc170f613ac3497d0bb8ed1a0339fbf0e
Unknown <waljesusme2013@gmail.com>
2017-12-18 21:21:19 -0800

    Exce. 123

---
+++ index.js
@@ -0,0 +1,38 @@
+// function totalPrice(sqEdge, pricePerSqFt) {
+
+//     return (sqEdge * pricePerSqFt);
+
+// };
+
+// console.log(totalprice(10,100));
+
+let day = "06";
+let month = "june";
+let year = "1980";
+
+let myBirthday = day + " " + month + " " + year;
+console.log(myBirthday);
+
+
+let age = 15;
+
+if (age > 10) {
+    console.log("Not permitted")}
+
+  else if (age> 15) {
+        console.log("Permitted wirth parent")
+    }
+    else if (age> 18) {
+        console.log("Permitted to attend alone")
+    }
+    else if (age< 18){
+        console.log("Permitted to attend alone")
+    };
+
+
+
+
+
+
+
+
